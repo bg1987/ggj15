d56a8e658a4d31a8abc42c1492e89b71677cd468
bg1987 <bg1987@gmail.com>
2015-01-23 19:33:12 +0200

    spikes checkpoints and stuff

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Checkpoint : MonoBehaviour {
+    public CheckpointManager manager;
+ 	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("checkpoint: "+other.gameObject);
+        if (other.tag != "Player")
+        {
+            return;
+        }
+        manager.SetCheckpoint(transform);
+        
+    }
+
+    
+}
---
+++ Assets/Scripts/CheckpointManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointManager : MonoBehaviour {
+
+    private Transform checkpoint;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag != "Player")
+        {
+            return;
+        }
+
+        other.gameObject.transform.position = checkpoint.position;
+    }
+
+
+    public void SetCheckpoint(Transform ckpt)
+    {
+        Debug.Log("SetCheckpoint: " + ckpt);
+
+        checkpoint = ckpt;
+    }
+}
